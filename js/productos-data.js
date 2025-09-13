448b1cbfb8bb898a70d1b11f5ab462625830c45d
jorgegalvez <jo.galvezr@duocuc.cl>
2025-09-13 12:40:29 -0300

    cambios a rama-admin

---
+++ js/productos-data.js
@@ -0,0 +1,100 @@
+const productos = [
+    // Tortas
+    {
+        categoria: "Tortas",
+        nombre: "Torta Cuadrada Chocolate",
+        precio: 45000,
+        imagen: "img/tortas/torta-chocolate.webp"
+    },
+    {
+        categoria: "Tortas",
+        nombre: "Torta Cuadrada Frutas",
+        precio: 50000,
+        imagen: "img/tortas/fruta-tortawebp.webp"
+    },
+    {
+        categoria: "Tortas",
+        nombre: "Torta Circular Vainilla",
+        precio: 40000,
+        imagen: "img/tortas/vainilla-circular.webp"
+    },
+    {
+        categoria: "Tortas",
+        nombre: "Torta Circular Manjar",
+        precio: 42000,
+        imagen: "img/tortas/manjar-redondawebp.webp"
+    },
+    {
+        categoria: "Tortas",
+        nombre: "Torta Sin Azucar Naranja",
+        precio: 48000,
+        imagen: "img/tortas/naranja-torta.webp"
+    },
+    {
+        categoria: "Tortas",
+        nombre: "Torta Vegana Chocolate",
+        precio: 50000,
+        imagen: "img/tortas/chocolate-vegan.webp"
+    },
+    {
+        categoria: "Tortas",
+        nombre: "Torta Especial Cumpleaños",
+        precio: 55000,
+        imagen: "img/tortas/cumpleanos-torta.webp"
+    },
+    {
+        categoria: "Tortas",
+        nombre: "Torta Especial Boda",
+        precio: 60000,
+        imagen: "img/tortas/boda-torta.webp"
+    },
+    // Postres
+    {
+        categoria: "Postres",
+        nombre: "Mousse Chocolate",
+        precio: 5000,
+        imagen: "img/postres/mousse-chocolate.webp"
+    },
+    {
+        categoria: "Postres",
+        nombre: "Tiramisú Clásico",
+        precio: 5500,
+        imagen: "img/postres/tiramisu-clasico.webp"
+    },
+    {
+        categoria: "Postres",
+        nombre: "Cheesecake Sin Azúcar",
+        precio: 47000,
+        imagen: "img/postres/cheescake-noazucar.webp"
+    },
+    {
+        categoria: "Postres",
+        nombre: "Empanada de Manzana",
+        precio: 3000,
+        imagen: "img/postres/empanada-manzana.webp"
+    },
+    {
+        categoria: "Postres",
+        nombre: "Tarta Santiago",
+        precio: 6000,
+        imagen: "img/postres/tarta-santiago.webp"
+    },
+    {
+        categoria: "Postres",
+        nombre: "Brownie Sin Gluten",
+        precio: 4000,
+        imagen: "img/postres/brownie.webp"
+    },
+    {
+        categoria: "Postres",
+        nombre: "Pan Sin Gluten",
+        precio: 3500,
+        imagen: "img/postres/pan-nogluten.jpg"
+    },
+    {
+        categoria: "Postres",
+        nombre: "Galletas Veganas de Avena",
+        precio: 4000,
+        imagen: "img/postres/galletas-avena.webp"
+    }
+];
